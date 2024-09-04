ffb7bc78bc3aa1bcfc05e368b8781b1bfc91ca1a
axbuglak <alexeybuglak24@gmail.com>
2024-09-04 23:48:26 +0200

    initial

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters')
-const dotenv = require('dotenv')
+'use strict';
 
-dotenv.config()
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
+const { request } = require('./lib/connect');
+const dotenv = require('dotenv');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+dotenv.config();
+const API_URL = process.env.API_URL;
+const send = request(API_URL);
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
